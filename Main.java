fa4be79c636c3c29c9502697fa92275ca88ebfbc
ansrlgur12 <ansrlgur12@naver.com>
2023-03-13 09:07:36 +0900

    문기혁은 확실히 바보가 아님

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Java");
         System.out.println("최수진 바보");
-        System.out.println("문기혁 바보");
+        System.out.println("문기혁은 바보가 아닙니다.");
     }
 }
